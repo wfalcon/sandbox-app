1328235631f3e0af92a8848f16b0635664d60c0a
Falcon <mail@sokolvlad.ru>
2022-03-22 19:24:52 +0300

    conlole.log

--- src/component/TodoItem/TodoItem.tsx
+++ src/component/TodoItem/TodoItem.tsx
@@ -5,13 +5,14 @@ import "./TodoItem.css"
 
 function TodoItem(props: any) {
   const [todosCompleted, setTodosComplited] = useState(props.item.completed)
-  // if (todosCompleted) {  
-  // console.log({todosCompleted})
-  // }
+
   const [todoClass, setTodoClass] =useState('todo-item')
+
   return (
-   <div onMouseOver={() => setTodoClass('todo-item-1')} onMouseOut={() => setTodoClass('todo-item')} className={todoClass}>
-     <input type="checkbox" checked={todosCompleted} onChange={()=>setTodosComplited(!todosCompleted)} />
+   <div onClick={()=>((setTodosComplited(!todosCompleted), console.log(todosCompleted, props.item.id)))} onMouseOver={() => setTodoClass('todo-item-highlight')} onMouseOut={() => setTodoClass('todo-item')} className={todoClass}>
+
+     <input type="checkbox" checked={todosCompleted} onChange={()=>{}} /> {/* onChange={()=>(setTodosComplited(!todosCompleted), console.log(todosCompleted))} /> */}
+     
      <p>{props.item.text}</p>
      
    </div>
