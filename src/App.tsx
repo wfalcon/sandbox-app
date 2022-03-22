19fc7be1c18d443458744f103b00804ad0ae79d3
Falcon <mail@sokolvlad.ru>
2022-03-22 21:41:19 +0300

    through

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
   const todoItems = todosData.map(item => <TodoItem key={item.id} item={item}/>)
   return (
     <div className='todo-list'  >
+      <h2 className='todolist'>Список дел</h2>
         {todoItems }
     </div>
 )
--- src/component/TodoItem/TodoItem.tsx
+++ src/component/TodoItem/TodoItem.tsx
@@ -6,14 +6,19 @@ import "./TodoItem.css"
 function TodoItem(props: any) {
   const [todosCompleted, setTodosComplited] = useState(props.item.completed)
 
-  const [todoClass, setTodoClass] =useState('todo-item')
+  const [todoClass, setTodoClass] = useState('todo-item')
+
 
   return (
-   <div onClick={()=>((setTodosComplited(!todosCompleted), console.log(todosCompleted, props.item.id)))} onMouseOver={() => setTodoClass('todo-item-highlight')} onMouseOut={() => setTodoClass('todo-item')} className={todoClass}>
+   <div onClick={()=>((setTodosComplited(!todosCompleted),
+    console.log(todosCompleted, props.item.id)))}
+    onMouseOver={() => setTodoClass('todo-item-highlight')}
+    onMouseOut={() => setTodoClass('todo-item')}
+    className={todoClass}>
 
-     <input type="checkbox" checked={todosCompleted} onChange={()=>{}} /> {/* onChange={()=>(setTodosComplited(!todosCompleted), console.log(todosCompleted))} /> */}
+  <input type="checkbox" checked={todosCompleted} onChange={()=>{}} /> {/* onChange={()=>(setTodosComplited(!todosCompleted), console.log(todosCompleted))} /> */}
      
-     <p>{props.item.text}</p>
+  <p className={todosCompleted ? "through" : ""} >{props.item.text}</p>
      
    </div>
   );
